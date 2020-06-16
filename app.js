0054eecea2f321c1cc60859891d3495a6a6a4393
AhmadHirthani <hertani86@gmail.com>
2020-06-16 12:27:16 +0300

    addFunctions

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function Branch(branchName, minCust, maxCust, avgCookSale) {
         for (var j = 0; j < 16; j++) {
             this.cookiesList[j] = Math.floor(this.avgCookSale * getRandomNumber(this.minCust, this.maxCust));
             this.totalCookies = this.totalCookies + this.cookiesList[j];
-            this.totalCookiesPerDay+=this.cookiesList[j];
+            this.totalCookiesPerDay += this.cookiesList[j];
             this.stafCountArray[j] = Math.ceil(this.cookiesList[j] / 20);
         }
     };
@@ -39,105 +39,97 @@ var tokyo = new Branch('Tokyo', 3, 24, 1.2);
 var dubai = new Branch('Dubai', 11, 38, 3.7);
 var paris = new Branch('Paris', 20, 38, 2.3);
 var lima = new Branch('Lima', 2, 16, 4.6);
-console.log('seattle: '+seattle);
-console.log('Total per day for seattle: '+seattle.totalCookiesPerDay);
+console.log('seattle: ' + seattle);
+console.log('Total per day for seattle: ' + seattle.totalCookiesPerDay);
 
 
 var main = document.createElement("main");
 document.body.appendChild(main);
 
-var salesHeader=document.createElement('h2');
+var salesHeader = document.createElement('h2');
 salesHeader.textContent = 'Sales';
 main.append(salesHeader);
 
 var table = document.createElement("table");
+table.setAttribute('id','slaesTable')
 main.appendChild(table);
 
-var headerTr = document.createElement('tr');
-table.appendChild(headerTr);
-
-var cornerTd = document.createElement('td');
-headerTr.appendChild(cornerTd);
-
-//create table's header automatically
-for (var i = 1; i < 17; i++) {
-    var headerTd = document.createElement('td');
-    if (i > 12) {
-        headerTd.textContent = i - 12 + 'pm ';
-    } else {
-        headerTd.textContent = i + 'am ';
-    }
-    headerTr.appendChild(headerTd);
-}
-
-var cornerTotalTd = document.createElement('td');
-cornerTotalTd.textContent='Totals';
-headerTr.appendChild(cornerTotalTd);
-
-
-
-console.log(seattle);
-
+createTableHeader();
 
 //loop on branhes's array to create the body of the table
 branches.forEach(myFunction);
 function myFunction(item, index) {
-    var totalCookiesPerDay=0;
-    for(var i=0;i<item.cookiesList.length;i++){
-        totalCookiesPerDay+=item.cookiesList[i];
+    var totalCookiesPerDay = 0;
+    for (var i = 0; i < item.cookiesList.length; i++) {
+        totalCookiesPerDay += item.cookiesList[i];
     }
-    //console.log('index: '+index+ ' item: '+ item.stafCountArray);
-    //document.getElementById("demo").innerHTML += index + ":" + item + "<br>"; 
     var tr = document.createElement('tr');
     table.appendChild(tr);
     var tdd = document.createElement('td');
     tdd.textContent = item.branchName;
     tr.appendChild(tdd);
-
-
     for (var x = 0; x < item.cookiesList.length; x++) {
         var td = document.createElement('td');
         td.textContent = item.cookiesList[x];
         tr.appendChild(td);
         sumsArray[x] += item.cookiesList[x];
     }
-
     var totalTd = document.createElement('td');
     totalTd.textContent = totalCookiesPerDay;
-        tr.appendChild(totalTd);
+    tr.appendChild(totalTd);
     totalCookiesPerDay
 }
 
+createTableFooter();
+
+function createTableFooter(){
 //create the footer of the table
 var footerTr = document.createElement('tr');
 table.appendChild(footerTr);
 var totalTd = document.createElement('td');
 totalTd.textContent = 'Totals';
 footerTr.appendChild(totalTd);
-var totalSum=0;
+var totalSum = 0;
 for (var i = 0; i < sumsArray.length; i++) {
     var headerTd = document.createElement('td');
     headerTd.textContent = sumsArray[i];
-    totalSum+=sumsArray[i];
+    totalSum += sumsArray[i];
     footerTr.appendChild(headerTd);
 }
 
 var totalSumTd = document.createElement('td');
 totalSumTd.textContent = totalSum;
 footerTr.appendChild(totalSumTd);
-
-
-var para=document.getElementById('para');
-para.textContent="fffff";
-
+var para = document.getElementById('para');
 var table1 = document.createElement("table");
-para.appendChild(table1);
-
 var headerTr1 = document.createElement('tr');
 table1.appendChild(headerTr1);
-
 var cornerTd1 = document.createElement('td');
 headerTr1.appendChild(cornerTd1);
+}
+
+//create table's header automatically
+function createTableHeader() {
+    //var table=document.getElementById('slaesTable');
+    var headerTr = document.createElement('tr');
+    headerTr.setAttribute('id', 'headerTr')
+    table.appendChild(headerTr);
+
+    var cornerTd = document.createElement('td');
+    headerTr.appendChild(cornerTd);
+    for (var i = 6; i < 22; i++) {
+        var headerTd = document.createElement('td');
+        if (i > 12) {
+            headerTd.textContent = i - 12 + 'pm ';
+        } else {
+            headerTd.textContent = i + 'am ';
+        }
+        headerTr.appendChild(headerTd);
+    }
+    var cornerTotalTd = document.createElement('td');
+    cornerTotalTd.textContent = 'Daily Location Total';
+    headerTr.appendChild(cornerTotalTd);
+}
 
 //create table's header automatically
 // for (var i = 1; i < 17; i++) {
